6074346cc64b03743a58fa2a7911615a15c3d806
dip4k <dip4k.chauhan@outlook.com>
2022-09-10 16:23:59 +0530

    First commit

---
+++ Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PPShowcase.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+        [DataType(DataType.Currency)]
+        public decimal UnitPrice { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime ManufacturingDate { get; set; }
+        public bool IsActive { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime AddedDate { get; set; }
+        public DateTime? LastModificationTime { get; set; }
+
+        public int CategoryId { get; set; }
+        public virtual Category Category { get; set; }
+    }
+}
